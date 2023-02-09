97c756f1677e33aed3f0fc153404b349f54ad990
goodness <goodyc474@gmail.com>
2023-02-09 01:09:29 +0100

    add functionality

--- src/calc/button/index.jsx
+++ src/calc/button/index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./style.css";
 
-const Button = ({ children, className, isOperator }) => {
+const Button = ({ children, className, isOperator, onClick }) => {
   return (
     <button
       className={`button ${isOperator ? "button_operator" : ""}${
         className || ""
       }`}
+      onClick={onClick}
     >
       <span>{children}</span>
     </button>
--- src/calc/index.jsx
+++ src/calc/index.jsx
@@ -76,15 +76,63 @@ const Calc = () => {
   const [current, setCurrent] = useState("0");
   const [previous, setPrevious] = useState("0");
 
+  const handleClick = (buttonText, isOperator) => {
+    console.log(buttonText, isOperator);
+
+    if (isOperator) {
+      if (buttonText === "clear") {
+        setCurrent("0");
+        setPrevious("0");
+        return;
+      }
+
+      if (buttonText === "c") {
+        setCurrent("0");
+        return;
+      }
+      if (buttonText === "Del") {
+        if (current.length === 1) {
+          setCurrent("0");
+          return;
+        }
+        setCurrent((current) => current.slice(0, -1));
+        return;
+      }
+
+      setPrevious((prev) => {
+        if (current === "0") return prev;
+
+        if (prev !== 0) {
+          const result = "hello " + prev;
+          return result;
+        }
+
+        return current + buttonText;
+      });
+      setCurrent("0");
+      return;
+    }
+    if (current === "0") {
+      setCurrent(buttonText);
+      return;
+    }
+    setCurrent(current + buttonText);
+  };
+
   return (
     <div className="calc">
       <div className="calc_display">
-        <div className="calc_display_current">0</div>
-        <div className="calc_display_previous">0</div>
+        <div className="calc_display_current">{current}</div>
+        <div className="calc_display_previous">{previous}</div>
       </div>
       <div className="calc_button">
         {buttonCon.map(({ text, isOperator, className }) => (
-          <Button key={text} isOperator={isOperator} className={className}>
+          <Button
+            key={text}
+            isOperator={isOperator}
+            className={className}
+            onClick={() => handleClick(text, isOperator)}
+          >
             {text}
           </Button>
         ))}
